d23074fcfeb410706b86257b576207161b6d0319
Aaron <bullett023@gmail.com>
2023-02-23 23:56:39 +0000

    refactored to increase human readability

---
+++ modules/getPayloadWithUnpackedToken.js
@@ -0,0 +1,19 @@
+// The getPayloadWithUnpackedToken module retrieves the auth token stored in memorty within the clientAuthTokenLibrary.
+// The auth token is used in a GET request to obtain the payload/XML for the client.
+
+import clientCredentials from "../config/clientLibrary.js"
+
+export async function getPayloadFor(nameOfClient, authTokenOfClient){
+    const identity = clientCredentials.find((key) => key.client_name === nameOfClient)
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${authTokenOfClient}`
+        }
+    };
+
+    const response = await fetch(identity.target(identity.brand_id, options))
+
+    return response
+}
