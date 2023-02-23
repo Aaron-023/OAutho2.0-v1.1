6c006f007c6f9f89bd0d81e0939b39efe1639b2d
Aaron <bullett023@gmail.com>
2023-02-23 23:36:07 +0000

    added clientAuthTokenLibrary

---
+++ config/clientAuthTokenLibrary.js
@@ -0,0 +1,5 @@
+// This clientAuthTokenLibrary stores obtained tokens in application memory.
+// The auth tokens are referenced in order to retrieve the XML payload.
+
+
+export const clientAuthTokens = {};
